bed32349f54a253bed2e3ca5268692697d5767ad
chlgudwnss <catigerlion2@gmail.com>
2025-04-06 21:42:30 +0900

    Add files via upload

---
+++ 0406.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+//한줄 주석
+
+void main(void) 
+{
+	/*첫번째 작성한 C 프로그램입니다.
+	2025.04.06
+	*/
+	printf("Hello! \n");
+	printf("I am a Student. \n");
+	printf("%d\n", 1234);
+	printf("%d\n	%d\n", 10, 20);
+	return 1; 
